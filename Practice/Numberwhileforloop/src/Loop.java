814d58ea3f8dbcf66aac526c21a789b0a30cf155
Ryanuw11 <rwd00007@mix.wvu.edu>
2024-02-28 14:32:07 -0500

    Resubmission for full version pushed

--- Practice/Numberwhileforloop/src/Loop.java
+++ Practice/Numberwhileforloop/src/Loop.java
@@ -3,25 +3,24 @@ public class Loop {
 
 	public static void main(String[] args) {
 		int loopsum =0;
-		
+		int loopsum2 = 5050;
 	for (int loopoh=1; loopoh<=100; loopoh++) {
 		loopsum += loopoh;
 			
 			
 			System.out.println(loopoh +" " +loopsum);
+			
 		
-		}
-		}
+	}
+
+	for (int loopin = 100; loopin > 1; loopin--) {
+		loopsum2 -= loopin;
 		
-	
-	
-	
-	
-	
-	
-	
-	
+		System.out.println(loopin-1 + " "+loopsum2);
+		
+	}
 	
 	
 	
 	}
+}
