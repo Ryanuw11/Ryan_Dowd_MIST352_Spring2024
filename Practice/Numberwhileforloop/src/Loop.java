80f304c18ddc003c8463ba0931df8b6df66fef68
Ryanuw11 <rwd00007@mix.wvu.edu>
2024-02-28 13:46:31 -0500

    ForLoop extra credit 1-100 pushed

---
+++ Practice/Numberwhileforloop/src/Loop.java
@@ -0,0 +1,27 @@
+
+public class Loop {
+
+	public static void main(String[] args) {
+		int loopsum =0;
+		
+	for (int loopoh=1; loopoh<=100; loopoh++) {
+		loopsum += loopoh;
+			
+			
+			System.out.println(loopoh +" " +loopsum);
+		
+		}
+		}
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
